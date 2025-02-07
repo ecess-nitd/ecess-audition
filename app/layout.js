46999284b7c545951ea76715bfb21d085647a0f8
Ayush Gupta <ayushgupta01195@gmail.com>
2025-02-07 19:49:29 +0530

    Add Vercel Analytics integration to layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono, Orbitron } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   );
